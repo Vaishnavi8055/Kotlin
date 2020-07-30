91e67b199c2ffb87d67aadc4d53c19d767c34a21
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-31 07:41:35 +1400

    Syntax of Declaring Variables in kotlin

---
+++ src/variabledeclaration/DeclarationPart2.kt
@@ -0,0 +1,15 @@
+package variabledeclaration
+
+//New Syntax Of declaration of variable in Kotlin
+fun main() {
+
+    var language: String = "Kotlin"
+    var number: Int = 25
+    var character: Char = 'K'
+    var decimal: Double = 65.6
+
+    val string: String = "Java"
+    val numbers: Int = 67
+    val characters: Char = 'V'
+    val float: Float = 61.90f
+}
