73591a2634adcba7f12f6749bb8f0cc9ac3dccd8
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-31 08:23:14 +1400

    Operators used to assign a value are Comparision Operators

---
+++ src/Operators/AssignmentOperators.kt
@@ -0,0 +1,29 @@
+package Operators
+
+fun main() {
+    var number1 = 14.8
+    val number2 = 2.2
+
+    number1 += number2
+    println(number1)
+
+    var number3 = 78.2
+
+    number3 -= number2
+    println(number3)
+
+    var number4 = 12.4
+
+    number4 *= number3
+    println(number4)
+
+    var number5 = 21.4
+
+    number5 /= number4
+    println(number5)
+
+    var number6 = 2.9
+
+    number6 %= number5
+    println(number6)
+}
