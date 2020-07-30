21a8364358596cb1b5df5157a4177cb02b56cb24
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-31 08:14:30 +1400

    Operators used to compare two Numbers are Comparision Operators

---
+++ src/Operators/ComparisionOperators.kt
@@ -0,0 +1,27 @@
+package Operators
+
+fun main() {
+    val number1 = 14.8
+    val number2 = 6.4
+
+    var result: Boolean
+
+    result = number1 == number2
+    println(result)
+
+    result = number1 != number2
+    println(result)
+
+    result = number1 <= number2
+    println(result)
+
+    result = number1 >= number2
+    println(result)
+
+    result = number1 > number2
+    println(result)
+
+    result = number1 < number2
+    println(result)
+
+}
