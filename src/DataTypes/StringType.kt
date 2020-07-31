5eabcca305c26bd62f5907e7a085480424b49738
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-31 21:17:56 +1400

    length of a String can be determined using length() method which is defined method in kotlin

---
+++ src/DataTypes/StringType.kt
@@ -0,0 +1,23 @@
+package DataTypes
+
+/*
+ * Created by Vaishnavi Chaurasia
+ * 31-Jul-20
+ * 12:42 PM
+ * Kotlin
+*/
+
+fun main() {
+
+    val string = "Welcome to kotlin Language !"
+    val stringLength: Int = string.length;
+
+    println(stringLength)
+
+    println("...................")
+
+
+
+
+
+}
