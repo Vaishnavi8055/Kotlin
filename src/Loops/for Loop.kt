8364a6ae132b0fbad1f84d06aa3699511210dd16
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-08-01 04:55:44 +1400

    for loop used to iterate within a range

---
+++ src/Loops/for Loop.kt
@@ -0,0 +1,17 @@
+package Loops
+
+/*
+ * Created by Vaishnavi Chaurasia
+ * 31-July-20
+ * 8:08 PM
+ * Kotlin
+*/
+
+fun main() {
+
+    // Syntax of for loop
+
+    for (i in 1..10) {        // No need to declare iterator in for loop
+        println(i)
+    }
+}
