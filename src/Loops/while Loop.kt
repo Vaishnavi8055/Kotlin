163e6865ca1ecc6c062344847148e566d594c7a9
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-08-01 05:00:42 +1400

    while loop will run until the condition is true

---
+++ src/Loops/while Loop.kt
@@ -0,0 +1,19 @@
+package Loops
+
+/*
+ * Created by Vaishnavi Chaurasia
+ * 31-July-20
+ * 8:26 AM
+ * Kotlin
+*/
+
+fun main() {
+
+    var i = 1;
+
+    // Syntax of while Loop
+    while (i <= 10) {
+        println(i)
+    }
+
+}
