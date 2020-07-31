676ab9fac19b6ee73a6c90829f67667d661d30c3
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-08-01 04:31:55 +1400

    Logical Operators are used for logical operations

---
+++ src/Operators/LogicalOperators.kt
@@ -0,0 +1,44 @@
+package Operators
+
+/*
+ * Created by Vaishnavi  Chaurasia
+ * 01-Aug-20
+ * 4:18 AM
+ * Kotlin
+*/
+
+fun main() {
+
+    val number1 = 0
+
+    //Example 1
+    if (number1 > 0 || number1 < 0) {
+        println("It is a Non Zero Integer")         //Logical OR ||
+    } else {
+        println("Integer is Zero")
+    }
+
+    //Example 2
+    if (number1 >= 0 && number1 <= 5) {
+        println("The value of Integer is between o-5")  //Logical AND &&
+    } else {
+        println("The value of Integer is not between o-5")
+    }
+
+    //Example 3
+    if (number1 != 0 && number1 <= 5) {
+        println("The value of Integer is between o-5")   //Logical NOT EQUAL TO !=
+    } else {
+        println("The value of Integer is not between o-5")
+    }
+
+    //Example 4
+    val number2 = 21
+    when (number2) {                                  // in is used to specify the range
+        in 10..15 -> println("Between 10-15")
+        in 16..20 -> println("Between 16-20")
+        in 21..25 -> println("Between 21-25")
+        in 26..30 -> println("Between 26-30")
+    }
+
+}
