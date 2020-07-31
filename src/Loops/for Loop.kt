7790e6ee33841d0f4c9327c3847dba85d9ddcd84
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-08-01 05:13:43 +1400

    Syntax of for Loop

--- src/Loops/for Loop.kt
+++ src/Loops/for Loop.kt
@@ -14,4 +14,20 @@ fun main() {
     for (i in 1..10) {        // No need to declare iterator in for loop
         println(i)
     }
+
+    println()
+
+    val array = arrayOf('K', 'V', 'A', 'P')
+
+    for (i in 0..array.size - 1) {
+        println("${array[i]}")
+    }
+
+    println()
+
+    for (i in 0 until array.size) {      // use of until
+        println("${array[i]}")
+    }
+
+
 }
