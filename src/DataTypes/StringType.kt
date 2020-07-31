cfffc22eada07d4ee388a3b410f2594b7a4afd51
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-08-01 00:05:07 +1400

    get(index) method returns a character at an index given in parenthesis

--- src/DataTypes/StringType.kt
+++ src/DataTypes/StringType.kt
@@ -12,12 +12,12 @@ fun main() {
     val string = "Welcome to kotlin Language !"
     val stringLength: Int = string.length;
 
-    println(stringLength)
+    println(stringLength)                       //length() method
 
     println("...................")
 
+    val stringIndex = string.get(13)            //get(index) method
 
-
-
-
+    print("The character at index 13 is :- ")
+    println(stringIndex)
 }
