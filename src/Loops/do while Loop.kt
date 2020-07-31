b893b4465c5d568537efdf217a327248979eb20c
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-08-01 05:05:50 +1400

    do while loop will run atleast once and then until the condition is true

--- src/Loops/do while Loop.kt
+++ src/Loops/do while Loop.kt
@@ -2,19 +2,16 @@ package Loops
 
 /*
  * Created by Vaishnavi Chaurasia
- * 31-July-20
- * 8:26 AM
+ * 01-Aug-20
+ * 5:01 AM
  * Kotlin
 */
 
 fun main() {
 
     var i = 1;
-
-    // Syntax of while Loop
-    while (i <= 10) {
+    do {
         println(i)
         i++
-    }
-
+    } while (i <= 10)
 }
