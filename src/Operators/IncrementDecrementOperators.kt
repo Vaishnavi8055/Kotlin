c8caeea132ee65dfff2ab2324d1e1da57cbd756f
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-07-31 21:04:34 +1400

    Operator used to increment or decrement a value by one is Increment or Decrement Operator respectively

---
+++ src/Operators/IncrementDecrementOperators.kt
@@ -0,0 +1,40 @@
+package Operators
+
+/*
+ * Created by Vaishnavi Chaurasia
+ * 31-Jul-20
+ * 12:20 PM
+ * Kotlin
+*/
+
+fun main() {
+    var number1 = 10
+    var number2 = 5
+
+    number1++            /* postfix Increment and Decrement Operator */
+    println(number1)
+
+    number1--
+    println(number1)
+
+    number2--
+    println(number2)
+
+    number2++
+    println(number2)
+
+    println()
+
+    ++number1           /* prefix Increment and Decrement Operator */
+    println(number1)
+
+    --number1
+    println(number1)
+
+    --number2
+    println(number2)
+
+    ++number2
+    println(number2)
+
+}
